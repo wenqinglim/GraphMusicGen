e7c54e656ab0aea2a976ebb5ac1bd1fcdd6f2cf0
wenqinglim <lim_wenq@hotmail.com>
2024-08-01 23:46:25 +0000

    Update structure gen model

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 conda create --prefix ~/GraphMusicGen/envs python=3.7
-conda activate /home/jovyan/GraphMusicGen/envs
+# conda activate /home/jovyan/GraphMusicGen/envs
+source activate /home/jovyan/GraphMusicGen/envs
 
+
+# To add conda env as jupyter kernel:
+python -m ipykernel install --user --name=graphmugen
